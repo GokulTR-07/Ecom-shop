5481a93063f6639c03b8df9840d029739f6e0752
GokulTR-07 <gokultr1206@gmail.com>
2024-03-03 00:33:09 +0530

    final change

--- backend/controller/user.js
+++ backend/controller/user.js
@@ -43,7 +43,7 @@ router.post("/create-user", upload.single("file"), async(req,res,next)=> {
 
   const activationToken = createActivationToken(user);
 
-  const activationUrl = `http://localhost:3000/activation/${activationToken}`;
+  const activationUrl = `https://ecom-shop-z8uk.vercel.app/activation/${activationToken}`;
     try {
       await sendMail({
         email: user.email,

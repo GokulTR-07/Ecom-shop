b567798da9041e7a5f89e8585e9b33f74cf4ccad
GokulTR-07 <gokultr1206@gmail.com>
2024-03-03 20:29:03 +0530

    link updated

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,3 +1,3 @@
-export const server = "https://ecom-shop-flax.vercel.app/v2"
+export const server = "https://ecom-shop-flax.vercel.app/api/v2"
 
 export const backend_url = "https://ecom-shop-flax.vercel.app/"

3e1a3e41fd39309f718541d64bbc0db613a783d1
GokulTR-07 <gokultr1206@gmail.com>
2024-03-09 14:58:21 +0530

    bug fixed

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,4 +1,4 @@
-export const server = "https://ecom-shop-flax.vercel.app/api/v2"
+export const server = "https://ecom-shop-topaz.vercel.app/api/v2"
 
 // export const backend_url = "https://ecom-shop-flax.vercel.app/"
 
